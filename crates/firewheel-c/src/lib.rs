2413372eeec2a565ab3fce9b3a08e7be851503b5
olekspickle <22867443+olekspickle@users.noreply.github.com>
2026-01-29 17:35:54 +0100

    Revert "maybe this will fix tests? generates duplicated function in header tho"
    This reverts commit 3f74f0e195dcd64d642ca4866cac1ee6084976d4.

--- crates/firewheel-c/src/lib.rs
+++ crates/firewheel-c/src/lib.rs
@@ -666,10 +666,10 @@ pub unsafe extern "C" fn fw_stream_config_set(
 }
 
 /// Create a list of available audio devices.
-#[cfg(feature = "cpal")]
 #[no_mangle]
 pub extern "C" fn fw_audio_device_list_create(input: bool) -> *mut FwAudioDeviceList {
     let enumerator = Backend::enumerator();
+    #[cfg(feature = "cpal")]
     let devices: Vec<String> = if input {
         enumerator
             .default_host()
@@ -685,18 +685,7 @@ pub extern "C" fn fw_audio_device_list_create(input: bool) -> *mut FwAudioDevice
             .map(|d| d.name.unwrap_or_default())
             .collect()
     };
-    let devices: Vec<CString> = devices
-        .into_iter()
-        .map(|d| CString::new(d).unwrap_or_default())
-        .collect();
-    let list = Box::new(devices);
-    Box::into_raw(list) as *mut FwAudioDeviceList
-}
-
-#[cfg(feature = "rtaudio")]
-#[no_mangle]
-pub extern "C" fn fw_audio_device_list_create(input: bool) -> *mut FwAudioDeviceList {
-    let enumerator = Backend::enumerator();
+    #[cfg(feature = "rtaudio")]
     let devices: Vec<String> = if input {
         enumerator
             .default_api()
